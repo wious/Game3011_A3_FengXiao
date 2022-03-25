237ab5a0e84e06c988b2ea47463772622d7ad417
Sakuya <sakuya16yweious@gmail.com>
2022-03-24 23:50:39 -0400

    initial project

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+	public static BoardManager instance;
+	public List<Sprite> easyFoods = new List<Sprite>();
+	public List<Sprite> mediumFoods = new List<Sprite>();
+	public List<Sprite> hardFoods = new List<Sprite>();
+	private List<Sprite> sources = new List<Sprite>();
+	public GameObject tile;
+	public int xSize, ySize;
+
+	private int difficulty = 0;
+	private GameObject[,] tiles;
+
+	public bool IsShifting { get; set; }
+
+	void Start()
+	{
+		instance = GetComponent<BoardManager>();
+	}
+
+	public void StartMinigame(int dLevel)
+    {
+		difficulty = dLevel;
+		Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
+		switch (difficulty)
+        {
+			case 1:
+				sources = new List<Sprite>(easyFoods);
+				break;
+			case 2:
+				sources = new List<Sprite>(mediumFoods);
+				break;
+			case 3:
+				sources = new List<Sprite>(hardFoods);
+				break;
+			default:
+				sources = new List<Sprite>(easyFoods);
+				break;
+		}
+		CreateBoard(offset.x, offset.y);
+	}
+
+
+
+	private void CreateBoard(float xOffset, float yOffset)
+	{
+		tiles = new GameObject[xSize, ySize];
+
+		float startX = transform.position.x;
+		float startY = transform.position.y;
+
+		Sprite[] previousLeft = new Sprite[ySize]; 
+		Sprite previousBelow = null; 
+
+		for (int x = 0; x < xSize; x++)
+		{
+			for (int y = 0; y < ySize; y++)
+			{
+				GameObject newTile = Instantiate(tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), tile.transform.rotation);
+				tiles[x, y] = newTile;
+				newTile.name = "X" + x + "Y" + y;
+				newTile.transform.parent = transform; 
+				List<Sprite> possibleCharacters = new List<Sprite>();
+				possibleCharacters.AddRange(sources);
+				possibleCharacters.Remove(previousLeft[y]);
+				possibleCharacters.Remove(previousBelow);
+				Sprite newSprite = possibleCharacters[Random.Range(0, possibleCharacters.Count)];
+				newTile.GetComponent<SpriteRenderer>().sprite = newSprite;
+				previousLeft[y] = newSprite;
+				previousBelow = newSprite;
+			}
+		}
+	}
+
+	private Sprite GetNewSprite(int x, int y)
+	{
+		List<Sprite> possibleFoods = new List<Sprite>();
+		possibleFoods.AddRange(sources);
+
+		if (x > 0)
+		{
+			possibleFoods.Remove(tiles[x - 1, y].GetComponent<SpriteRenderer>().sprite);
+		}
+        if (x < xSize - 1)
+        {
+            possibleFoods.Remove(tiles[x + 1, y].GetComponent<SpriteRenderer>().sprite);
+        }
+        if (y > 0)
+		{
+			possibleFoods.Remove(tiles[x, y - 1].GetComponent<SpriteRenderer>().sprite);
+		}
+        if (y < ySize - 1)
+        {
+            possibleFoods.Remove(tiles[x, y + 1].GetComponent<SpriteRenderer>().sprite);
+        }
+        return possibleFoods[Random.Range(0, possibleFoods.Count)];
+	}
+
+	private IEnumerator MoveTilesDown(int x, int yStart, float shiftDelay = .03f)
+	{
+		IsShifting = true;
+		List<SpriteRenderer> renders = new List<SpriteRenderer>();
+		int nullCount = 0;
+
+		for (int y = yStart; y < ySize; y++)
+		{
+			SpriteRenderer render = tiles[x, y].GetComponent<SpriteRenderer>();
+			if (render.sprite == null)
+			{
+				nullCount++;
+			}
+			renders.Add(render);
+		}
+
+		for (int i = 0; i < nullCount; i++)
+		{
+			GameManager.instance.PlaySFX();
+			yield return new WaitForSeconds(shiftDelay);
+			for (int k = 0; k < renders.Count - 1; k++)
+			{
+				renders[k].sprite = renders[k + 1].sprite;
+				renders[k + 1].sprite = GetNewSprite(x, ySize - 1);
+			}
+			if (yStart == ySize - 1)
+			{
+				renders[0].sprite = GetNewSprite(x, ySize - 1);
+			}
+		}
+		IsShifting = false;
+	}
+
+	public IEnumerator FindNullTiles()
+	{
+		for (int x = 0; x < xSize; x++)
+		{
+			for (int y = 0; y < ySize; y++)
+			{
+				if (tiles[x, y].GetComponent<SpriteRenderer>().sprite == null)
+				{
+					yield return StartCoroutine(MoveTilesDown(x, y));
+					break;
+				}
+			}
+		}
+
+		for (int x = 0; x < xSize; x++)
+		{
+			for (int y = 0; y < ySize; y++)
+			{
+				tiles[x, y].GetComponent<Tile>().ClearAllMatches();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameObject menuPanel;
+    public GameObject winPanel;
+    public GameObject losePanel;
+    public bool isStart = false;
+    public AudioSource source;
+
+
+    public int dLevel = 0;
+    private int timer = 0;
+    public int pointCounter = 0;
+
+    public TMP_Text timerText;
+    public TMP_Text moveCounterText;
+    public TMP_Text instructionText;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        menuPanel.SetActive(true);
+        winPanel.SetActive(false);
+        losePanel.SetActive(false);
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+    }
+
+    public void PlaySFX()
+    {
+        source.Play();
+    }
+
+    private void Update()
+    {
+        timerText.text = timer.ToString();
+        moveCounterText.text = pointCounter.ToString();
+        WinConfition();
+        LoseCondition();
+    }
+
+    private void WinConfition()
+    {
+        if (isStart && pointCounter >= 30)
+        {
+            winPanel.SetActive(true);
+        }
+    }
+
+    private void LoseCondition()
+    {
+        if (isStart && pointCounter < 30 && timer <= 0)
+        {
+            losePanel.SetActive(true);
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Minigame");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void EasyLevel()
+    {
+        isStart = true;
+        timer = 65;
+        menuPanel.SetActive(false);
+        dLevel = 1;
+        instructionText.text = "Match 3";
+        BoardManager.instance.StartMinigame(dLevel);
+        StartCoroutine(countTime(1.0f));
+    }
+    public void MediumLevel()
+    {
+        isStart = true;
+        timer = 55;
+        menuPanel.SetActive(false);
+        dLevel = 2;
+        instructionText.text = "Match 4";
+        BoardManager.instance.StartMinigame(dLevel);
+        StartCoroutine(countTime(1.0f));
+    }
+    public void HardLevel()
+    {
+        isStart = true;
+        timer = 45;
+        menuPanel.SetActive(false);
+        dLevel = 3;
+        instructionText.text = "Match 5 ";
+        BoardManager.instance.StartMinigame(dLevel);
+        StartCoroutine(countTime(1.0f));
+    }
+
+    private IEnumerator countTime(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        timer--;
+        StartCoroutine(countTime(1.0f));
+    }
+}
